e9efbf06388d22f52f48874e95f5eb90f218935c
yangxi <599321378@qq.com>
2019-04-09 15:24:28 +0800

    feat: 升级api使用方式

--- readme.md
+++ readme.md
@@ -10,43 +10,68 @@
 $ npm i --save @monajs/rn-px2dp
 ```
 
-## 代码演示
+## demo1
 
 ```js
 import Px2dp from '@monajs/rn-px2dp'
 
-const styles = StyleSheet.create(new Px2dp({
+const styles = StyleSheet.create(px2dp({
+	container: {
+        flex: 1,
+        fontSize: 20,
+        margin: 10,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#F5FCFF',
+    },
+	welcome: {
+		fontSize: 20,
+		textAlign: 'center',
+		margin: 10
+	},
+}, {
 	uiWidthPx: 375,
 	props: ['borderWidth']
-}).run({
-	container: {
-		flex: 1,
-		fontSize: 20,
-		margin: 10,
-		justifyContent: 'center',
-		alignItems: 'center',
-		backgroundColor: '#F5FCFF',
-	}
 }))
 ```
 
+## demo2
+
+```js
+import Px2dp from '@monajs/rn-px2dp'
+
+<Text style={{fontSize: px2dp._px2dp(20, 375)}}>@monajs/rn-px2dp</Text>
+```
+
 ## API
-### new Px2dp([config])
+### px2dp(styles, config)
 
 | 参数 | 类型 | 默认值 | 描述 |
 | --- | --- | --- | :-- |
+| styles | `Object` | | 转换前的样式对象 |
 | [config] | `Object` | | |
 | [config.uiWidthPx] | `Number` | `750` | 设计稿总宽度 |
 | [config.props] | `Array` | | 需要自定义转换的样式属性 |
 
-### Px2dp.run(styles)
-开始执行转换
+**props**: 默认会转换以下的样式属性，可以通过设置`props`来进行赋能
+```js
+const _propList = [
+	'width', 'height',
+	'top', 'right', 'bottom', 'left',
+	'margin', 'marginTop', 'marginRight', 'marginBottom', 'marginLeft',
+	'padding', 'paddingTop', 'paddingRight', 'paddingBottom', 'paddingLeft',
+	'borderRadius', 'borderTopLeftRadius', 'borderTopRightRadius', 'borderBottomLeftRadius', 'borderBottomRightRadius',
+	'shadowRadius',
+	'fontSize'
+]
+```
 
-**Kind**: instance method of [`Px2dp`]
+### px2dp._px2dp(uiElementPx, uiWidthPx)
 
-| 参数 | 类型 | 描述 |
-| --- | -- | :-- |
-| styles | `Object` | 转换前的样式对象 |
+| 参数 | 类型 | 默认值 | 描述 |
+| --- | --- | --- | :-- |
+| uiElementPx | `Number` | | 设计稿元素宽度 |
+| uiWidthPx | `Number` | `750` | 设计稿总宽度 |
 
 ## 联系我
 > 微信：599321378
--- src/index.js
+++ src/index.js
@@ -4,51 +4,47 @@
  * 原理：元素所占屏幕宽度的比例是相同的
  * 我们需要做的是降视觉稿宽度(dp)换算成不同屏幕尺寸下对应的宽度(dp)
  * 公式：设计稿元素宽度（px） / 设计稿总宽度（px） = 元素的宽度（dp） / 屏幕的总宽度（dp）
- * @param uiElementPx
- * @returns {number}
+ * @param styles
+ * @param config
+ * @returns {styles}
  */
 
 import { Dimensions } from 'react-native'
 
-const propList = [
+const _propList = [
 	'width', 'height',
 	'top', 'right', 'bottom', 'left',
-	'marginTop', 'marginRight', 'marginBottom', 'marginLeft',
-	'paddingTop', 'paddingRight', 'paddingBottom', 'paddingLeft',
+	'margin', 'marginTop', 'marginRight', 'marginBottom', 'marginLeft',
+	'padding', 'paddingTop', 'paddingRight', 'paddingBottom', 'paddingLeft',
 	'borderRadius', 'borderTopLeftRadius', 'borderTopRightRadius', 'borderBottomLeftRadius', 'borderBottomRightRadius',
 	'shadowRadius',
 	'fontSize'
 ]
 
-class Px2dp {
-	constructor (config = {}) {
-		this.uiWidthPx = 750
-		this.props = propList
-		const { uiWidthPx, props = [] } = config
+// 设备 dp 宽度
+const _deviceWidthDp = Dimensions.get('window').width
 
-		if (uiWidthPx) {
-			this.uiWidthPx = uiWidthPx
-		}
-
-		if (props.length > 0) {
-			this.props = this.props.concat(props)
-		}
-	}
-
-	run (styles = {}) {
-		const deviceWidthDp = Dimensions.get('window').width
-
-		for (let className in styles) {
-			const classItem = styles[className]
-			for (let prop in classItem) {
-				if (classItem.hasOwnProperty(prop) && this.props.includes(prop) && typeof classItem[prop] === 'number') {
-					classItem[prop] = classItem[prop] * deviceWidthDp / this.uiWidthPx
-				}
-			}
-		}
-
-		return styles
-	}
+const _px2dp = function (uiElementPx, uiWidthPx = 750) {
+	return uiElementPx * _deviceWidthDp / uiWidthPx
 }
 
-export default Px2dp
+const px2dp = function (styles = {}, config = {}) {
+	let { uiWidthPx = 750, props = [] } = config
+
+	props = _propList.concat(props)
+
+	for (let className in styles) {
+		const classItem = styles[className]
+		for (let prop in classItem) {
+			if (classItem.hasOwnProperty(prop) && props.includes(prop) && typeof classItem[prop] === 'number') {
+				classItem[prop] = _px2dp(classItem[prop], uiWidthPx)
+			}
+		}
+	}
+
+	return styles
+}
+
+px2dp._px2dp = _px2dp
+
+module.exports = px2dp
